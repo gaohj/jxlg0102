8d2669dec7912fc2ac74891134b7e122e6a75db6
gaohj <gaohj@126.com>
2019-06-20 09:35:08 +0800

    day8

---
+++ day08/model_form_demo/front/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import  AddBookForm
+from .models import Book
+# Create your views here.
+
+def add_book(request):
+    form = AddBookForm(request.POST)
+    if form.is_valid():
+        title = form.cleaned_data.get('title')
+        page = form.cleaned_data.get('page')
+        price = form.cleaned_data.get('price')
+        print("title:%s" % title)
+        print("page:%s" % page)
+        print("price:%s" % price)
+        form.save() # 模型表单中 可以直接使用save保存到数据库中
+        # book = Book.objects.create(title=title)
+        return HttpResponse('SUCCESS')
+    else:
+        print(form.errors.get_json_data())
+        return HttpResponse('fail')
